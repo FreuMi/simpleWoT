4f522b43ddf8a6d590bcd3f2ea944b41a92acaeb
Michael Freund <michaelfreund95@gmail.com>
2026-04-01 09:30:12 +0200

    Update GAP

--- src/simplewot/bindings/ble_gap.py
+++ src/simplewot/bindings/ble_gap.py
@@ -7,10 +7,19 @@ scan_lock = asyncio.Lock()
 
 async def listen(forms) -> bytes | None:
     target_mac = forms["target"].split("://")[1].replace("-",":")
-    raw_bytes = await get_gap_advertisement(target_mac)
+    manufacturer_id = None
+
+    # Check for manufacuring id
+    if len(target_mac.split("/")) != 1:
+        split_data = target_mac.split("/")
+        target_mac = split_data[0]
+        manufacturer_id = split_data[-1]
+        manufacturer_id = int(manufacturer_id, 16) # parse as int
+
+    raw_bytes = await get_gap_advertisement(target_mac, manufacturer_id)
     return raw_bytes
 
-async def get_gap_advertisement(target_mac: str) -> bytes | None:
+async def get_gap_advertisement(target_mac: str, manufacturer_id: int | None) -> bytes | None:
     found_event = asyncio.Event()
     TIME_OUT = 15.0 
     value = None
@@ -23,11 +32,17 @@ async def get_gap_advertisement(target_mac: str) -> bytes | None:
         if not adv_data.manufacturer_data:
             return
         
-        for company_id, raw_bytes in adv_data.manufacturer_data.items():                    
-            value = raw_bytes
-            break 
+        for company_id, raw_bytes in adv_data.manufacturer_data.items(): 
+            if manufacturer_id == None:               
+                value = raw_bytes
+                found_event.set()
+                return 
+            else:
+                if manufacturer_id == company_id:
+                    value = raw_bytes
+                    found_event.set()
+                    return 
                 
-        found_event.set()
     
     async with scan_lock:
         # Give BlueZ a tiny buffer to finish previous D-Bus teardowns
